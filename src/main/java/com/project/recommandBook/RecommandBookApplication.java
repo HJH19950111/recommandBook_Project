737bfd79b8c4a84384f3f1cc3a457aa911ce6885
HJH19950111 <infobackuphjh@gmail.com>
2023-03-29 16:30:57 +0900

    Initial commit

---
+++ src/main/java/com/project/recommandBook/RecommandBookApplication.java
@@ -0,0 +1,13 @@
+package com.project.recommandBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommandBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommandBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/recommandBook/RecommandBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.recommandBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommandBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
